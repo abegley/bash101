f4a8c52d6c08999109a7d6e2c02b20e896e2f9ef
Alan <begleyalan@gmail.com>
2014-03-26 21:34:35 +0000

    Added new chapter for control structures, basic for command

---
+++ 02/forLoop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#for command - basic format is as follow
+#
+#for var in list
+#do
+#     commands
+#done
+
+
+for test in Alabama Alaska Arizona Arkansas California Colorado
+do 
+    echo "The next state is $test"
+done
+
